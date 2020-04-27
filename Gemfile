53113650c1203c01ada2ff05d7e98785a513a2f7
shinsuke <shinst710@gmail.com>
2020-04-27 18:19:05 +0900

    implement rspec test

--- Gemfile
+++ Gemfile
@@ -8,10 +8,18 @@ gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
 gem 'bootsnap',   '1.4.4', require: false
+gem 'slim-rails'
+gem 'html2slim'
 
 group :development, :test do
   gem 'sqlite3', '1.4.1'
   gem 'byebug',  '11.0.1', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'rails-controller-testing'
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-byebug'
 end
 
 group :development do
@@ -23,13 +31,6 @@ end
 
 group :test do
   gem 'capybara',                 '3.28.0'
-  gem 'selenium-webdriver',       '3.142.4'
-  gem 'webdrivers',               '4.1.2'
-  gem 'rails-controller-testing', '1.0.4'
-  gem 'minitest',                 '5.11.3'
-  gem 'minitest-reporters',       '1.3.8'
-  gem 'guard',                    '2.16.2'
-  gem 'guard-minitest',           '2.4.6'
 end
 
 group :production do
---
+++ spec/requests/access_to_static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Access to static_pages', type: :request do
+
+  context 'GET #home' do
+    before { get root_path }
+    it 'responds successfully' do
+      expect(response).to have_http_status 200
+    end
+    it "has title 'Ruby on Rails Tutorial Sample App'" do
+      expect(response.body).to include 'Ruby on Rails Tutorial Sample App'
+      expect(response.body).to_not include '| Ruby on Rails Tutorial Sample App'
+    end
+  end
+
+  context 'GET #help' do
+    before { get static_pages_help_path }
+    it 'responds successfully' do
+      expect(response).to have_http_status 200
+    end
+    it "has title 'Help | Ruby on Rails Tutorial Sample App'" do
+      expect(response.body).to include 'Help | Ruby on Rails Tutorial Sample App'
+    end
+  end
+
+  context 'GET #about' do
+    before { get static_pages_about_path }
+    it 'responds successfully' do
+      expect(response).to have_http_status 200
+    end
+    it "has title 'About | Ruby on Rails Tutorial Sample App'" do
+      expect(response.body).to include 'About | Ruby on Rails Tutorial Sample App'
+    end
+  end
+end
