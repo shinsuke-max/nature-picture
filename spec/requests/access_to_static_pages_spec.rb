ce87eb662063f5fc8b49e50c631ceb5c83aa09b3
shinsuke <shinst710@gmail.com>
2020-04-28 13:37:05 +0900

    finish ch05

--- spec/requests/access_to_static_pages_spec.rb
+++ spec/requests/access_to_static_pages_spec.rb
@@ -8,28 +8,38 @@ RSpec.describe 'Access to static_pages', type: :request do
       expect(response).to have_http_status 200
     end
     it "has title 'Ruby on Rails Tutorial Sample App'" do
-      expect(response.body).to include 'Ruby on Rails Tutorial Sample App'
+      expect(response.body).to include full_title('')
       expect(response.body).to_not include '| Ruby on Rails Tutorial Sample App'
     end
   end
 
   context 'GET #help' do
-    before { get static_pages_help_path }
+    before { get help_path }
     it 'responds successfully' do
       expect(response).to have_http_status 200
     end
     it "has title 'Help | Ruby on Rails Tutorial Sample App'" do
-      expect(response.body).to include 'Help | Ruby on Rails Tutorial Sample App'
+      expect(response.body).to include full_title('Help')
     end
   end
 
   context 'GET #about' do
-    before { get static_pages_about_path }
+    before { get about_path }
     it 'responds successfully' do
       expect(response).to have_http_status 200
     end
     it "has title 'About | Ruby on Rails Tutorial Sample App'" do
-      expect(response.body).to include 'About | Ruby on Rails Tutorial Sample App'
+      expect(response.body).to include full_title('About')
+    end
+  end
+
+  context 'GET #contact' do
+    before { get contact_path }
+    it 'responds successfully' do
+      expect(response). to have_http_status 200
+    end
+    it "has title 'Contact | Ruby on Rails Tutorial Sample App'" do
+      expect(response.body).to include full_title('Contact')
     end
   end
 end
---
+++ spec/systems/site_layout_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'site layout', type: :system do
+  context 'access to root_path' do
+    before { visit root_path }
+    subject { page }
+    it 'has links such as root_path, help_path, contact_path and about_path' do
+      is_expected.to have_link nil, href: root_path, count: 2
+      is_expected.to have_link 'Help', href: help_path
+      is_expected.to have_link 'About', href: about_path
+      is_expected.to have_link 'Contact', href: contact_path
+      is_expected.to have_link 'Sign up now!', href: signup_path
+    end
+  end
+
+  context 'access to about_path' do
+    before { visit about_path }
+    subject { page }
+    it 'has links such as root_path, help_path, contact_path and about_path' do
+      is_expected.to have_link nil, href: root_path, count: 2
+      is_expected.to have_link 'Help', href: help_path
+      is_expected.to have_link 'About', href: about_path
+      is_expected.to have_link 'Contact', href: contact_path
+    end
+  end
+end
